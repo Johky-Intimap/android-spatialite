9add88bcc197f316f70728d092f7986ff8f22c79
JOHKY-LEGION\johky <johky@intimap.com>
2024-04-02 14:08:33 +0700

    x19

--- android-spatialite/src/main/jni/libspatialite/src/headers/spatialite/gaiaconfig-msvc.h
+++ android-spatialite/src/main/jni/libspatialite/src/headers/spatialite/gaiaconfig-msvc.h
@@ -3,6 +3,7 @@
 
 /* Should be defined in order to enable GCP support. */
 /* #undef ENABLE_GCP */
+#define ENABLE_GCP 1
 
 /* Should be defined in order to enable GeoPackage support. */
 #define ENABLE_GEOPACKAGE 1
